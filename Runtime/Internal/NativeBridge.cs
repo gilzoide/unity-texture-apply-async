03d92cf30e56bd8a919d822ffbd532d856fc29c3
gilzoide <gilzoide@gmail.com>
2024-02-24 19:05:49 -0300

    Make NativeBridge internal instead of public

--- Runtime/Internal/NativeBridge.cs
+++ Runtime/Internal/NativeBridge.cs
@@ -3,22 +3,22 @@ using System.Runtime.InteropServices;
 
 namespace Gilzoide.TextureAsyncApply.Internal
 {
-    public class NativeBridge
+    internal class NativeBridge
     {
 #if UNITY_WEBGL && !UNITY_EDITOR
-        public const string LibraryName = "__Internal";
+        internal const string LibraryName = "__Internal";
 #else
-        public const string LibraryName = "texture_async_apply";
+        internal const string LibraryName = "texture_async_apply";
 #endif
 
         [DllImport(LibraryName, EntryPoint = "TextureAsyncApply_event_handler")]
-        public static extern IntPtr GetEventHandler();
-        public static readonly IntPtr EventHandler_ptr = GetEventHandler();
+        internal static extern IntPtr GetEventHandler();
+        internal static readonly IntPtr EventHandler_ptr = GetEventHandler();
 
         [DllImport(LibraryName, EntryPoint = "TextureAsyncApply_new")]
-        public static extern uint RegisterHandle(IntPtr buffer);
+        internal static extern uint RegisterHandle(IntPtr buffer);
 
         [DllImport(LibraryName, EntryPoint = "TextureAsyncApply_dispose")]
-        public static extern void UnregisterHandle(uint handle);
+        internal static extern void UnregisterHandle(uint handle);
     }
 }
